4d15417fb264426ee92bbb4b101f9492631a3697
Tanjil Hassan <kenk14382@gmail.com>
2024-10-19 02:08:52 +0600

    all setup is done and checking git is working properly

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <>
       <h1 className="text-red-800 text-3xl bg-black">Vite + React</h1>
+      <h1 className="text-red-800 text-2xl">Checking if the git is working properly or not</h1>
     </>
   );
 }
